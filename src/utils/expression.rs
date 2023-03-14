c5a168a48d0afc9e1613c9f5759dcc73c3a3fc51
peterstrongg <peterstrongdev@outlook.com>
2023-03-14 16:04:16 -0400

    parsing progress

--- src/utils/expression.rs
+++ src/utils/expression.rs
@@ -1,17 +1,46 @@
+use super::token;
+
 pub struct Expression {
-    
+    operator: token::Token,
+    left: Box<Expression>,
+    right: Box<Expression>
+}
+
+impl Default for Expression {
+    fn default() -> Expression {
+        Expression {
+            operator: Default::default(),
+            left: Default::default(),
+            right: Default::default(),
+        }
+    }
 }
 
 impl Expression {
     pub fn new() -> Expression {
-        Expression {}
+        Expression { ..Default::default() }
     }
 }
 
 pub trait Binary {
-
+    fn binary(&mut self, l:Expression, op:token::Token, r:Expression);
 }
 
 pub trait Unary {
-
+    fn unary(&mut self, op:token::Token, r:Expression);
 }
+
+impl Binary for Expression {
+    fn binary(&mut self, l: Expression, op: token::Token, r: Expression) {
+        self.operator = op;
+        *self.left = l;
+        *self.right = r;
+    }
+}
+
+impl Unary for Expression {
+    fn unary(&mut self, op: token::Token, r: Expression) {
+        self.operator = op;
+        *self.right = r
+    }
+} 
